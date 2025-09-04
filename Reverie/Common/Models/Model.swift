8c0c3c84461d0c50856416b1fabc66ea54423d9c
BraydenHuguenard <brayden.huguenard@gmail.com>
2025-09-04 18:08:21 -0400

    Added Folder Seperation

---
+++ Reverie/Common/Models/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  Reverie
+//
+//  Created by Brayden Huguenard on 9/2/25.
+//
+
+// Dream Model
+// Dream
+// Dream returned by AI
+// Dream Analysis
+// Dream Tags
+// Dream Date
+// Image
+
+// User Model
+// User ID
+// Username
---
+++ Reverie/Logging/LoggingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoggingView.swift
+//  Reverie
+//
+//  Created by Brayden Huguenard on 9/4/25.
+//
+
+import SwiftUI
+
+struct LoggingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoggingView()
+}
