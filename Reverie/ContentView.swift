7b5586d1449831aa6bd57637e597054e644dfc37
BraydenHuguenard <brayden.huguenard@gmail.com>
2025-09-09 19:10:40 -0400

    reverie

--- Reverie/ContentView.swift
+++ Reverie/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! I'm here")
+            Text("Reverie!!!!!")
         }
         .padding()
     }
