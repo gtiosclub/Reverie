baeca3406a08eebb917e2b31233031bca38edb06
Zhihui Chen <zchen0630@gmail.com>
2025-09-18 18:22:36 -0400

    search bar

--- Reverie/Common/Views/SearchBarView.swift
+++ Reverie/Common/Views/SearchBarView.swift
@@ -1,17 +1,16 @@
 import SwiftUI
 
 struct SearchBarView: View {
-    @State private var searchText: String = ""
+    @State var searchText: String = ""
 
     var body: some View {
         HStack {
             Image(systemName: "magnifyingglass")
                 .foregroundColor(.gray)
             TextField("Search", text: $searchText)
-                .foregroundColor(.primary)
         }
         .padding()
-        .background(Color(.systemGray5))
+        .background(Color(.systemGray4))
         .cornerRadius(12)
         .padding()
     }
