61aaa75918b409a4f8e6a32122b8637c687d2663
Kapitar <kkapitar@ya.ru>
2025-09-23 18:57:58 -0400

    fix: add NavgationStack

--- Reverie/Logging/LoggingView.swift
+++ Reverie/Logging/LoggingView.swift
@@ -13,40 +13,42 @@ struct LoggingView: View {
     @State private var date = Date()
     
     var body: some View {
-        ZStack {
-            VStack() {
-                HStack {
-                    Spacer()
-                    NavigationLink {
-                        // TODO: Add navigation link
-                    } label: {
-                        Image(systemName: "checkmark")
-                            .font(.system(size: 24, weight: .bold))
-                            .foregroundColor(.black)
-                            .padding(6)
-                            .background(Circle().fill(.gray.opacity(0.9)))
-                            .padding(.vertical, 4)
-                            .opacity(title.isEmpty || dream.isEmpty ? 0 : 1)
+        NavigationStack {
+            ZStack {
+                VStack() {
+                    HStack {
+                        Spacer()
+                        NavigationLink {
+                            // TODO: Add navigation link
+                        } label: {
+                            Image(systemName: "checkmark")
+                                .font(.system(size: 24, weight: .bold))
+                                .foregroundColor(.black)
+                                .padding(6)
+                                .background(Circle().fill(.gray.opacity(0.9)))
+                                .padding(.vertical, 4)
+                                .opacity(title.isEmpty || dream.isEmpty ? 0 : 1)
+                        }
                     }
-                }
-                HStack {
-                    TextField("Dream Name", text: $title)
-                        .font(.title)
-                        .bold()
+                    HStack {
+                        TextField("Dream Name", text: $title)
+                            .font(.title)
+                            .bold()
+                        Spacer()
+                        DatePicker(
+                            "",
+                            selection: $date,
+                            displayedComponents: [.date]
+                        )
+                    }
+                    
+                    TextField("Start new dream entry...", text: $dream, axis: .vertical)
+                        .padding(.vertical)
                     Spacer()
-                    DatePicker(
-                        "",
-                        selection: $date,
-                        displayedComponents: [.date]
-                    )
                 }
-                
-                TextField("Start new dream entry...", text: $dream, axis: .vertical)
-                    .padding(.vertical)
-                Spacer()
+                .padding()
+                TabbarView()
             }
-            .padding()
-            TabbarView()
         }
     }
 }
