f65f2eb6473953d8d0cfc2fa317650647fbba5ab
Nithya <52676594+lunanith@users.noreply.github.com>
2025-09-26 15:51:57 -0400

    Firebase Fixed!!

--- Reverie/ReverieApp.swift
+++ Reverie/ReverieApp.swift
@@ -6,9 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct ReverieApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             NavigationStack {
@@ -17,3 +31,4 @@ struct ReverieApp: App {
         }
     }
 }
+
