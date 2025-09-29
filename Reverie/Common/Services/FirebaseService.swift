1c1f9cd34fd0c0b4b1103955423cd2076f0b1b2c
BraydenHuguenard <brayden.huguenard@gmail.com>
2025-09-29 18:01:55 -0400

    Firebase Updates

--- Reverie/Common/Services/FirebaseService.swift
+++ Reverie/Common/Services/FirebaseService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Firebase
+
 public class FirebaseService {
     let db = Firebase.db
     
--- Reverie/DreamCards/DreamCardView.swift
+++ Reverie/DreamCards/DreamCardView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct DreamCardView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         ZStack {
             BackgroundView()
             VStack {
                 Text("Dream Cards")
                     .foregroundStyle(Color(.white))
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.largeTitle)
+                        .foregroundColor(.gray)
+                        .padding()
+                }
             }
             TabbarView()
         }
