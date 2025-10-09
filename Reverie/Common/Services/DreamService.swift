08a3c46f37789f48485c48343e75c7e03d8da298
Nithya <52676594+lunanith@users.noreply.github.com>
2025-10-09 15:38:04 -0400

    add dream service

---
+++ Reverie/Common/Services/DreamService.swift
@@ -0,0 +1,13 @@
+//
+//  DreamService.swift
+//  Reverie
+//
+//  Created by Nithya Ravula on 10/9/25.
+//
+
+import Foundation
+
+class DreamService {
+    // for methods relating to getting details from dreams 
+    
+}
