838018160e97feec29f36c277c96eee5e210960b
sgarg330 <sgarg330@gatech.edu>
2025-10-12 13:20:23 -0400

    added emotion frequency dict

--- Reverie/Profile/ProfileView.swift
+++ Reverie/Profile/ProfileView.swift
@@ -98,6 +98,18 @@ func getDreamsOfCategory(dreams: [DreamModel], category: DreamModel.Tags) -> [Dr
     }
 }
 
+func findEmotionFrequency(dreams: [DreamModel]) -> [DreamModel.Emotions: Int] {
+    var emotionsDict = [DreamModel.Emotions: Int]()
+    
+    for d in dreams {
+        emotionsDict[d.emotion, default: 0] += 1
+    }
+    return emotionsDict
+}
+
+
+
+
 
 #Preview {
     ProfileView()
