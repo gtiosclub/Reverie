c46e4c55da084987ebe022516c20e10e3d2fdb79
Nithya <52676594+lunanith@users.noreply.github.com>
2025-10-21 17:13:25 -0400

    add dream

--- Reverie/Common/Models/DreamModel.swift
+++ Reverie/Common/Models/DreamModel.swift
@@ -19,6 +19,7 @@ class DreamModel: Decodable {
     var tags: [Tags]
     var image: String
     var emotion: Emotions
+    var finishedDream: String = "None"
     
     @Generable
     enum Tags: String, Codable, CaseIterable {
@@ -45,5 +46,19 @@ class DreamModel: Decodable {
         self.tags = tags
         self.image = image
         self.emotion = emotion
+        self.finishedDream = "None"
+    }
+    init(userID: String, id: String, title: String, date: Date, loggedContent: String, generatedContent:String, tags: [Tags], image: String, emotion: Emotions, finishedDream: String) {
+        self.userID = userID
+        self.id = id
+        self.title = title
+        self.date = date
+        self.title = title
+        self.loggedContent = loggedContent
+        self.generatedContent = generatedContent
+        self.tags = tags
+        self.image = image
+        self.emotion = emotion
+        self.finishedDream = finishedDream
     }
 }
