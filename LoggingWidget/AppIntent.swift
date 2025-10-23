4094f2135f5fbcc58cf5fd6e595615ecce6ae742
Kapitar <kkapitar@ya.ru>
2025-10-23 13:07:15 -0400

    feat: add a logging widget

---
+++ LoggingWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  LoggingWidget
+//
+//  Created by Artem Kim on 10/21/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
