1e9398cc5098173fe56606535ef397f146f672e8
Nithya <52676594+lunanith@users.noreply.github.com>
2025-10-30 18:53:12 -0400

    fixed issues (thank brayden)

--- Reverie/Home/NewLogView.swift
+++ Reverie/Home/NewLogView.swift
@@ -10,31 +10,29 @@ import SwiftUI
 struct NewLogView: View {
     
     var body: some View {
-        NavigationStack {
-            HStack {
-                Text("New Log")
-                    .font(.system(size: 18))
-                    .bold()
-                    .padding(.leading, 16)
+        HStack {
+            Text("New Log")
+                .font(.system(size: 18))
+                .bold()
+                .padding(.leading, 16)
+                .foregroundColor(.white)
+            Spacer()
+            NavigationLink {
+                LoggingView()
+            } label: {
+                Image(systemName: "plus")
+                    .font(.system(size: 12, weight: .bold))
                     .foregroundColor(.white)
-                Spacer()
-                NavigationLink {
-                    LoggingView()
-                } label: {
-                    Image(systemName: "plus")
-                        .font(.system(size: 12, weight: .bold))
-                        .foregroundColor(.white)
-                        .padding(6)
-                        .background(Circle().fill(.gray.opacity(0.9)))
-                        .padding(.horizontal, 12)
-                }
+                    .padding(6)
+                    .background(Circle().fill(.gray.opacity(0.9)))
+                    .padding(.horizontal, 12)
             }
-            .padding()
-            .frame(maxWidth: 300, minHeight: 50)
-            .glassEffect(.regular, in: .rect)
-            .cornerRadius(20)
-            .padding(.horizontal, 50)
         }
+        .padding()
+        .frame(maxWidth: 300, minHeight: 50)
+        .glassEffect(.regular, in: .rect)
+        .cornerRadius(20)
+        .padding(.horizontal, 50)
     }
 }
 
