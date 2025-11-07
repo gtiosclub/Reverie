d94ee5041f2ba89ef8757889c6b71b6464e9907b
Divya Mathew <divyamathew965@gmail.com>
2025-11-07 16:20:16 -0500

    updated home screen and new log views

--- Reverie/Home/FloatingStickersView.swift
+++ Reverie/Home/FloatingStickersView.swift
@@ -11,6 +11,7 @@ struct FloatingStickersView: View {
 //    @Environment(FirebaseDCService.self) private var fbdcs
     @State private var characters: [CardModel] = []
 
+
     var body: some View {
         GeometryReader { geometry in
             ZStack {
