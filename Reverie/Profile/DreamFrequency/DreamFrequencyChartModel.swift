5abee319b666a4c2f207575521b6473c51b651ba
BraydenHuguenard <brayden.huguenard@gmail.com>
2025-11-13 21:57:25 -0500

    News Dream Frequency Chart SwiftUI Charts

---
+++ Reverie/Profile/DreamFrequency/DreamFrequencyChartModel.swift
@@ -0,0 +1,14 @@
+//
+//  DreamFrequencyChartModel.swift
+//  Reverie
+//
+//  Created by Brayden Huguenard on 11/13/25.
+//
+
+import SwiftUI
+
+struct DreamFrequencyChartModel: Identifiable {
+    let id = UUID()
+    let date: Date
+    let count: Int
+}
---
+++ Reverie/Profile/DreamFrequency/DreamFrequencyChartView.swift
@@ -0,0 +1,110 @@
+//
+//  DreamFrequencyChartView.swift
+//  Reverie
+//
+//  Created by Brayden Huguenard on 11/13/25.
+//
+
+import SwiftUI
+import Charts
+
+// dream dictionary, see DreamFrequencyChartModel
+let dreams: ([DreamFrequencyChartModel], Int) = CleanDreamDataService.shared.processDreamsIntoWeeklyCounts(dreams: FirebaseLoginService.shared.currUser?.dreams ?? [])
+
+// DEFAULT DATA
+let placeholderData: [DreamFrequencyChartModel] = [
+    .init(date: Calendar.current.date(byAdding: .day, value: -6, to: Date())!, count: 2),
+    .init(date: Calendar.current.date(byAdding: .day, value: -5, to: Date())!, count: 1),
+    .init(date: Calendar.current.date(byAdding: .day, value: -4, to: Date())!, count: 3),
+    .init(date: Calendar.current.date(byAdding: .day, value: -3, to: Date())!, count: 2),
+    .init(date: Calendar.current.date(byAdding: .day, value: -2, to: Date())!, count: 5),
+    .init(date: Calendar.current.date(byAdding: .day, value: -1, to: Date())!, count: 4),
+    .init(date: Date(), count: 3)
+]
+
+struct DreamFrequencyChartView: View {
+    
+    @State private var dreamData: [DreamFrequencyChartModel] = dreams.0
+    
+    @State private var last3WeeksAverage: Int = dreams.1 / 3
+    
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading, spacing: 6) {
+                HStack {
+                    Text(CleanDreamDataService.shared.trendText(allTimeCount: dreamData.count, ThreeWeekAvg: last3WeeksAverage))
+                        .font(.subheadline)
+                        .foregroundColor(.white.opacity(0.8))
+                        .multilineTextAlignment(.leading)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(.gray)
+                        .font(Font.system(size: 14))
+                }
+                .padding(.horizontal)
+                .padding(.top, 8)
+                .padding(.bottom, 3)
+                
+                Rectangle()
+                    .fill(Color.white.opacity(0.05))
+                    .frame(height: 1)
+                    .padding(.horizontal, 20)
+                    .padding(.top, 10)
+            }
+            
+            Chart {
+                ForEach(dreamData) { data in
+                    // line connecting data points
+                    LineMark(
+                        x: .value("Date", data.date, unit: .weekOfMonth),
+                        y: .value("Dreams", data.count)
+                    )
+                    // line smooth and curved
+                    .interpolationMethod(.catmullRom)
+                    .foregroundStyle(Color.indigo)
+
+                    // data point
+                    PointMark(
+                        x: .value("Date", data.date, unit: .weekOfMonth),
+                        y: .value("Dreams", data.count)
+                    )
+                    .foregroundStyle(.indigo)
+                    .symbolSize(CGSize(width: 8, height: 8))
+                }
+                
+                if last3WeeksAverage > 0 {
+                    RuleMark(
+                        y: .value("3 Week Avg", last3WeeksAverage)
+                    )
+                    .lineStyle(StrokeStyle(lineWidth: 2))
+                    .foregroundStyle(.indigo)
+                }
+                
+            }
+            .chartXAxis {
+                AxisMarks(values: .automatic(desiredCount: 7)) { value in
+                    AxisGridLine()
+                    AxisValueLabel(format: .dateTime.month(.abbreviated), centered: true)
+                }
+            }
+            .chartYAxis {
+                AxisMarks { value in
+                    AxisGridLine()
+                    AxisValueLabel()
+                }
+            }
+            .frame(height: 250)
+        }
+        .padding()
+        .glassEffect(in: .rect)
+        .cornerRadius(10)
+        .padding()
+    }
+}
+
+#Preview {
+    DreamFrequencyChartView()
+}
