7ac38fa771f756aabf1a5a6e9f385cf43062711d
sgarg330 <sgarg330@gatech.edu>
2025-11-14 04:20:48 -0500

    added color extension

--- Reverie/Common/Models/DreamModel.swift
+++ Reverie/Common/Models/DreamModel.swift
@@ -336,6 +336,22 @@ extension DreamModel.Emotions {
     }
 }
 
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >> 8) & 0xFF) / 255.0
+        let b = Double(rgb & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b)
+    }
+    
+    static let profileContainer = Color(hex: "#1D1C3A")
+}
+
+
 
 
 
