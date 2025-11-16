5db7a14dbf4043c0447b4ccd8de4c761f0d0463d
BraydenHuguenard <brayden.huguenard@gmail.com>
2025-11-16 16:41:56 -0500

    Changes for Demo

--- Reverie/Logging/SaveDreamView.swift
+++ Reverie/Logging/SaveDreamView.swift
@@ -443,13 +443,14 @@ struct SaveDreamView: View {
     
     func saveDream() async {
         do {
+            // newDream = try await FirebaseDreamService.shared.fetchDream(dream: PUT DREAM ID OF DREAM TO COPY!!!!)
             newDream.date = selectedDate
             var createdDreamID = try await FirebaseDreamService.shared.createDream(dream: newDream)
             
             createdDream = try await FirebaseDreamService.shared.fetchDream(dreamID: createdDreamID)
             FirebaseLoginService.shared.currUser?.dreams.append(createdDream!)
-            FirebaseDCService.shared.generateImage(for: createdDream!)
-            FirebaseDCService.shared.generateImageForDC(for: createdDream!)
+            FirebaseDCService.shared.generateImage(for: createdDream!) // COMMMENT THIS OUT WHEN HARD CODING
+            FirebaseDCService.shared.generateImageForDC(for: createdDream!) // COMMMENT THIS OUT WHEN HARD CODING
             navigateToDreamEntry = true
             
             updateTagDescriptions(tags: newDream.tags)
