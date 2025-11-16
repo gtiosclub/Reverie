76241973b086f75ccdf078be36912848039eb9d6
Isha <148280322+chimkenIJ@users.noreply.github.com>
2025-11-15 19:27:13 -0500

    nav bar fixes

--- Reverie/Profile/Heatmap/CombinedView.swift
+++ Reverie/Profile/Heatmap/CombinedView.swift
@@ -18,105 +18,116 @@ struct CombinedHeatmapEmotionView: View {
                 BackgroundView()
                     .ignoresSafeArea()
                 
-                VStack(spacing: 0) {
-                    
-                    HStack {
-                        Button(action: {
-                            dismiss()
-                        }) {
-                            ZStack {
-                                
-                                Circle()
-                                    .fill(
-                                        LinearGradient(
-                                            colors: [
-                                                Color(red: 5/255, green: 7/255, blue: 20/255),
-                                                Color(red: 17/255, green: 18/255, blue: 32/255)
-                                            ],
-                                            startPoint: .topLeading,
-                                            endPoint: .bottomTrailing
-                                        )
-                                    )
-                                    .frame(width: 55, height: 55)
-                                    .overlay(
-                                        Circle()
-                                            .strokeBorder(
-                                                AngularGradient(
-                                                    gradient: Gradient(colors: [
-                                                        Color.white.opacity(0.8),
-                                                        Color.white.opacity(0.1),
-                                                        Color.white.opacity(0.6),
-                                                        Color.white.opacity(0.1),
-                                                        Color.white.opacity(0.8)
-                                                    ]),
-                                                    center: .center
-                                                ),
-                                                lineWidth: 0.5
-                                            )
-                                            .blendMode(.screen)
-                                    )
-                                
-                                Image(systemName: "chevron.left")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 20, height: 20)
-                                    .foregroundColor(.white)
-                                    .padding(.leading, -4)
-                                    .bold(true)
-                            }
-                            
-                        }
-                        .buttonStyle(.plain)
-                        .padding(.leading, 8)
-                        
-                        Spacer()
-                        
-                        
-                        VStack(spacing: 2) {
-                            Text("Moods")
-                                .font(.system(size: 18, weight: .semibold))
-                                .foregroundColor(.white)
-                                .shadow(color: Color(red: 37/255, green: 23/255, blue: 79/255).opacity(0.7), radius: 4)
-                                .shadow(color: Color(red: 37/255, green: 23/255, blue: 79/255).opacity(0.3), radius: 8)
-                        }
-                        
-                        Spacer()
-                        
-                        Rectangle()
-                            .fill(Color.clear)
-                            .frame(width: 55, height: 55)
-                            .padding(.trailing, 8)
-                            .opacity(0)
-                    }
-                    
-                    .padding(.horizontal)
-                    .padding(.top, 10)
-                    .padding(.bottom, 4)
-                    
+              
                     
                     ScrollView {
                         VStack(spacing: 5) {
-                            moodSummary()
+                          /*  moodSummary()
                                 .font(.system(size: 14))
                                 .multilineTextAlignment(.leading)
                                 .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.horizontal, 18)
+                                .padding(.horizontal, 18)*/
                             HeatmapView()
                             renderEmotionCircles(from: dreams)
                         }
                         .padding()
-                        //.padding(.top, 80)
+                        .padding(.top, 70)
+                        //.padding(.bottom, 80)
+                        //.padding(.horizontal)
                     }
-                    // Spacer()
-                    
-                }
-                
-            }
-            
-            .navigationBarHidden(true)
-        }
-    }
-}
+                LinearGradient(
+                                   gradient: Gradient(colors: [
+                                       Color.black.opacity(0.9),
+                                       Color.black.opacity(0.6),
+                                       Color.black.opacity(0.3),
+                                       Color.black.opacity(0)
+                                   ]),
+                                   startPoint: .top,
+                                   endPoint: .bottom
+                               )
+                               .frame(height: 90)
+                               .ignoresSafeArea(edges: .top)
+                               .blendMode(.overlay)
+
+                               // Floating nav bar
+                               HStack {
+                                   Button(action: { dismiss() }) {
+                                       ZStack {
+                                           Circle()
+                                               .fill(
+                                                   LinearGradient(
+                                                       colors: [
+                                                           Color(red: 5/255, green: 7/255, blue: 20/255),
+                                                           Color(red: 17/255, green: 18/255, blue: 32/255)
+                                                       ],
+                                                       startPoint: .topLeading,
+                                                       endPoint: .bottomTrailing
+                                                   )
+                                               )
+                                               .frame(width: 55, height: 55)
+                                               .overlay(
+                                                   Circle()
+                                                       .strokeBorder(
+                                                           AngularGradient(
+                                                               gradient: Gradient(colors: [
+                                                                   Color.white.opacity(0.8),
+                                                                   Color.white.opacity(0.1),
+                                                                   Color.white.opacity(0.6),
+                                                                   Color.white.opacity(0.1),
+                                                                   Color.white.opacity(0.8)
+                                                               ]),
+                                                               center: .center
+                                                           ),
+                                                           lineWidth: 0.5
+                                                       )
+                                                       .blendMode(.screen)
+                                               )
+
+                                           Image(systemName: "chevron.left")
+                                               .resizable()
+                                               .scaledToFit()
+                                               .frame(width: 20, height: 20)
+                                               .foregroundColor(.white)
+                                               .padding(.leading, -4)
+                                               .bold(true)
+                                       }
+                                   }
+                                   .buttonStyle(.plain)
+                                   .padding(.leading, 8)
+
+                                   Spacer()
+
+                                   Text("Moods")
+                                       .font(.system(size: 18, weight: .semibold))
+                                       .foregroundColor(.white)
+                                       .shadow(color: Color(red: 37/255, green: 23/255, blue: 79/255).opacity(0.7), radius: 4)
+                                       .shadow(color: Color(red: 37/255, green: 23/255, blue: 79/255).opacity(0.3), radius: 8)
+
+                                   Spacer()
+
+                                   Rectangle()
+                                       .fill(Color.clear)
+                                       .frame(width: 55, height: 55)
+                                       .opacity(0)
+                               }
+                               .padding(.horizontal)
+                               .padding(.top, 8)
+                               .padding(.bottom, 4)
+                               .background(
+                                   LinearGradient(
+                                       gradient: Gradient(stops: [
+                                           .init(color: Color(hex: "#010023"), location: 0.0),
+                                           .init(color: Color.clear, location: 1.0)
+                                       ]),
+                                       startPoint: .top,
+                                       endPoint: .bottom
+                                   )
+                               )
+                           }
+                           .navigationBarHidden(true)
+                       }
+                   }
+               }
 
 
 #Preview {
